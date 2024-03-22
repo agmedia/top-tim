b472d6af26731ab2cedfa4574e15c6eed9c427dd
tomashina <tomislav@agmedia.hr>
2024-03-22 11:28:45 +0100

    push front translations

--- app/Models/Front/Blog.php
+++ app/Models/Front/Blog.php
@@ -21,11 +21,88 @@ class Blog extends Model
      * @var array
      */
     protected $guarded = ['id', 'created_at', 'updated_at'];
-    
+
     /**
-     * @var string[] 
+     * @var string[]
      */
     protected $appends = ['thumb'];
+
+    protected $locale = 'en';
+
+
+    /**
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->locale = current_locale();
+    }
+
+
+    /**
+     * @param $locale
+     *
+     * @return \Illuminate\Database\Eloquent\HigherOrderBuilderProxy|mixed
+     */
+    public function getLocalizedRouteKey($locale)
+    {
+        return $this->translation($locale)->slug;
+    }
+
+
+    /**
+     * @param $value
+     * @param $field
+     *
+     * @return Model|never|null
+     */
+    public function resolveRouteBinding($value, $field = NULL)
+    {
+        return static::whereHas('translation', function ($query) use ($value) {
+            $query->where('slug', $value);
+        })->first() ?? abort(404);
+    }
+
+
+    /**
+     * @param null  $lang
+     * @param false $all
+     *
+     * @return Model|\Illuminate\Database\Eloquent\Relations\HasMany|\Illuminate\Database\Eloquent\Relations\HasOne|object|null
+     */
+    public function translation($lang = null, bool $all = false)
+    {
+        if ($lang) {
+            return $this->hasOne(PageTranslation::class, 'page_id')->where('lang', $lang)->first();
+        }
+
+        if ($all) {
+            return $this->hasMany(PageTranslation::class, 'page_id');
+        }
+
+        return $this->hasOne(PageTranslation::class, 'page_id')->where('lang', $this->locale);
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getTitleAttribute()
+    {
+        return $this->translation->title;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getDescriptionAttribute()
+    {
+        return $this->translation->description;
+    }
+
 
 
     /**
@@ -37,8 +114,8 @@ class Blog extends Model
     {
         return 'slug';
     }
-    
-    
+
+
     /**
      * @param $value
      *
@@ -48,8 +125,8 @@ class Blog extends Model
     {
         return config('settings.images_domain') . str_replace('.jpg', '.webp', $value);
     }
-    
-    
+
+
     /**
      * @param $value
      *
