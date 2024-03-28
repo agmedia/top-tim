1e2bcecca53e173fa6bfff59d64a5277a7fba2d8
tomashina <tomislav@agmedia.hr>
2024-03-28 20:04:05 +0100

    search backend

--- resources/views/back/catalog/category/index.blade.php
+++ resources/views/back/catalog/category/index.blade.php
@@ -19,7 +19,7 @@
             <div class="content content-full">
             @include('back.layouts.partials.session')
                 <div id="accordion" role="tablist" aria-multiselectable="true">
-                    @forelse($categoriess as $group => $categories)
+                    @forelse($categoriess->sortBy('sort_order') as $group => $categories)
                         <h3 class="{{ ! $loop->first ? 'mt-5' : '' }}"><small class="font-weight-light">{{ __('back/categories.grupa_kategorija') }}: </small>{{ $group }} <small class="font-weight-light">{{ $categories->count() }}</small></h3>
 
                         @forelse($categories as $category)
