d91e6f8426c5bb919e68622052b42e3157346577
Filip Jankoski <filip@agmedia.hr>
2024-07-04 11:03:03 +0200

    _added: User_group_id in database.

---
+++ migrations/021_alter_table_product_actions_add_user_group_id.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `product_actions` ADD `user_group_id` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `date_end`;
---
+++ migrations/022_alter_table_user_details_add_user_group_id.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `user_details` ADD `user_group_id` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `user_id`;
