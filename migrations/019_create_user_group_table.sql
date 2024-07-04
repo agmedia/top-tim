cd496ad0088d454664c317a5466cc2eb7706886e
tomashina <tomislav@agmedia.hr>
2024-07-04 10:52:39 +0200

    user_group tables

---
+++ migrations/019_create_user_group_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user_group` (
+                           `id` bigint UNSIGNED NOT NULL,
+                           `parent_id` bigint NOT NULL,
+                           `status` tinyint(1) DEFAULT NULL,
+                           `created_at` timestamp NULL DEFAULT NULL,
+                           `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ migrations/020_create_user_group_translations_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user_group_translations` (
+                                         `id` bigint UNSIGNED NOT NULL,
+                                         `user_group_id` bigint UNSIGNED NOT NULL,
+                                         `lang` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'en',
+                                         `title` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                                         `created_at` timestamp NULL DEFAULT NULL,
+                                         `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
