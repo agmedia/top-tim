096dee61791aacbe19522f989e0833a9181b858d
tomashina <tomislav@agmedia.hr>
2024-08-12 13:58:16 +0200

    import medicinke

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -94,7 +94,7 @@ class DashboardController extends Controller
     public function import(Request $request)
     {
         $import = new Import();
-        $xml    = new \SimpleXMLElement($import->getFromURL('https://www.toptim.agmedia.rocks/lopte-pumpe.xml'));
+        $xml    = new \SimpleXMLElement($import->getFromURL('https://www.toptim.agmedia.rocks/lopte-medicinke.xml'));
         $count  = 0;
 
         foreach ($xml->row as $item) {
@@ -176,7 +176,7 @@ class DashboardController extends Controller
 
                             ProductAttribute::query()->insert([
                                 'product_id'  => $new_product_id,
-                                'attribute_id' => 29,
+                                'attribute_id' => 30,
                             ]);
 
                             $prod = Product::query()->find($new_product_id);
