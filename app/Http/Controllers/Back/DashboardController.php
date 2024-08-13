9a46abac70a18ded765ac0f46c330aa7c878d766
tomashina <tomislav@agmedia.hr>
2024-08-13 08:18:26 +0200

    fitnes

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -94,7 +94,7 @@ class DashboardController extends Controller
     public function import(Request $request)
     {
         $import = new Import();
-        $xml    = new \SimpleXMLElement($import->getFromURL('https://www.toptim.agmedia.rocks/balet.xml'));
+        $xml    = new \SimpleXMLElement($import->getFromURL('https://www.toptim.agmedia.rocks/fitnes.xml'));
         $count  = 0;
 
         foreach ($xml->row as $item) {
@@ -176,7 +176,7 @@ class DashboardController extends Controller
 
                             ProductCategory::query()->insert([
                                 'product_id'  => $new_product_id,
-                                'category_id' => 98,
+                                'category_id' => 97,
                             ]);
 
 
