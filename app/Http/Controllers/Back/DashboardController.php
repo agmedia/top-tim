a6566d326d9f169aa49bd05db8255636f0a0b8fa
tomashina <tomislav@agmedia.hr>
2024-08-13 09:26:52 +0200

    golovi

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -94,7 +94,7 @@ class DashboardController extends Controller
     public function import(Request $request)
     {
         $import = new Import();
-        $xml    = new \SimpleXMLElement($import->getFromURL('https://www.toptim.agmedia.rocks/ostalo.xml'));
+        $xml    = new \SimpleXMLElement($import->getFromURL('https://www.toptim.agmedia.rocks/golovi.xml'));
         $count  = 0;
 
         foreach ($xml->row as $item) {
@@ -176,13 +176,13 @@ class DashboardController extends Controller
 
                             ProductCategory::query()->insert([
                                 'product_id'  => $new_product_id,
-                                'category_id' => 91,
+                                'category_id' => 51,
                             ]);
 
 
                        ProductAttribute::query()->insert([
                                 'product_id'  => $new_product_id,
-                                'attribute_id' => 36,
+                                'attribute_id' => 37,
                             ]);
 
                             $prod = Product::query()->find($new_product_id);
