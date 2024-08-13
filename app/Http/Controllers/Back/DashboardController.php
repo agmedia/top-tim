b56bc821cbf7da3f2f8c0c327f1183c7b55b4ac9
tomashina <tomislav@agmedia.hr>
2024-08-13 08:22:05 +0200

    vaterpolo

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -94,7 +94,7 @@ class DashboardController extends Controller
     public function import(Request $request)
     {
         $import = new Import();
-        $xml    = new \SimpleXMLElement($import->getFromURL('https://www.toptim.agmedia.rocks/fitnes.xml'));
+        $xml    = new \SimpleXMLElement($import->getFromURL('https://www.toptim.agmedia.rocks/vaterpolo.xml'));
         $count  = 0;
 
         foreach ($xml->row as $item) {
@@ -176,7 +176,7 @@ class DashboardController extends Controller
 
                             ProductCategory::query()->insert([
                                 'product_id'  => $new_product_id,
-                                'category_id' => 97,
+                                'category_id' => 96,
                             ]);
 
 
