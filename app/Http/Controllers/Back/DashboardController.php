b7183f445e4d35349df5649d9856701bfde33686
tomashina <tomislav@agmedia.hr>
2024-08-13 10:13:29 +0200

    odbojka-ostalo

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -94,7 +94,7 @@ class DashboardController extends Controller
     public function import(Request $request)
     {
         $import = new Import();
-        $xml    = new \SimpleXMLElement($import->getFromURL('https://www.toptim.agmedia.rocks/klupice.xml'));
+        $xml    = new \SimpleXMLElement($import->getFromURL('https://www.toptim.agmedia.rocks/odbojka-ostalo.xml'));
         $count  = 0;
 
         foreach ($xml->row as $item) {
@@ -180,13 +180,13 @@ class DashboardController extends Controller
 
                             ProductCategory::query()->insert([
                                 'product_id'  => $new_product_id,
-                                'category_id' => 51,
+                                'category_id' => 92,
                             ]);
 
 
                        ProductAttribute::query()->insert([
                                 'product_id'  => $new_product_id,
-                                'attribute_id' => 42,
+                                'attribute_id' => 36,
                             ]);
 
                             $prod = Product::query()->find($new_product_id);
