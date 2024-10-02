9a06ef8922650914745680d1a70cee32b16374f8
tomashina <tomislav@agmedia.hr>
2024-10-02 09:14:44 +0200

    filter limit

--- resources/views/back/catalog/product/edit.blade.php
+++ resources/views/back/catalog/product/edit.blade.php
@@ -11,11 +11,11 @@
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
                 <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">{{ __('back/products.artikl_edit') }}</h1>
-
+                @if (isset($product))
                 <a class="flex btn btn-outline-primary" target="_blank" href="{{ url($product->translation->url) }}">
                     <i class="fa fa-fw fa-eye"></i>
                 </a>
-
+                @endif
 
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb">
