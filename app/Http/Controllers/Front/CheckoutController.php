72200b37ed0e8bdbdbed570f334383af42e09484
tomashina <tomislav@agmedia.hr>
2024-10-03 13:43:07 +0200

    mypos

--- app/Http/Controllers/Front/CheckoutController.php
+++ app/Http/Controllers/Front/CheckoutController.php
@@ -114,8 +114,16 @@ class CheckoutController extends FrontBaseController
     {
         $order = new Order();
 
-        //Log::info('Response Corvus::::::::::::::::::::::::::::::::::::::');
-       // Log::info($request->toArray());
+
+
+
+        if ($request->has('IPCmethod')) {
+            $toptim_orderid = str_replace('toptim','', $request->get('OrderID'));
+            $order->setData($toptim_orderid);
+
+            echo 'OK';
+        }
+
 
         if ($request->has('provjera')) {
             $order->setData($request->input('provjera'));
@@ -126,6 +134,11 @@ class CheckoutController extends FrontBaseController
         }
 
         if ($order->finish($request)) {
+
+            if ($request->has('IPCmethod')) {
+                return redirect()->route('checkout.success.mypos');
+            }
+
             return redirect()->route('checkout.success');
         }
 
@@ -211,45 +224,22 @@ class CheckoutController extends FrontBaseController
      */
     public function successMypos(Request $request): bool
     {
-        Log::info($request);
-        $cnf = new \Mypos\IPC\Config();
-        $cnf->setIpcURL('https://www.mypos.com/vmp/checkout-test/');
-        $cnf->setLang('hr');
-        $cnf->setVersion('1.4');
-        $configurationPackage = 'eyJzaWQiOiIwMDAwMDAwMDAwMDAwMTAiLCJjbiI6IjYxOTM4MTY2NjEwIiwicGsiOiItLS0tLUJFR0lOIFJTQSBQUklWQVRFIEtFWS0tLS0tXHJcbk1JSUNYQUlCQUFLQmdRQ2YwVGRjVHVwaGI3WCtad2VrdDFYS0VXWkRjelNHZWNmbzZ2UWZxdnJhZjVWUHpjbkpcclxuMk1jNUo3MkhCbTB1OThFSkhhbitubGUyV09aTVZHSXRUYVwvMmsxRlJXd2J0N2lRNWR6RGg1UEVlWkFTZzJVV2VcclxuaG9SOEw4TXBOQnFINmg3WklUd1ZUZlJTNExzQnZsRWZUN1B6aG01WUpLZk0rQ2R6RE0rTDlXVkVHd0lEQVFBQlxyXG5Bb0dBWWZLeHdVdEVicTh1bFZyRDNubldoRitoazFrNktlamRVcTBkTFlOMjl3OFdqYkNNS2I5SWFva21xV2lRXHJcbjVpWkdFcll4aDdHNEJEUDhBV1wvK005SFhNNG9xbTVTRWtheGhiVGxna3MrRTFzOWRUcGRGUXZMNzZUdm9kcVN5XHJcbmwyRTJCZ2hWZ0xMZ2tkaFJuOWJ1YUZ6WXRhOTVKS2ZneUtHb25OeHNRQTM5UHdFQ1FRREtiRzBLcDZLRWtOZ0Jcclxuc3JDcTNDeDJvZDVPZmlQREc4ZzNSWVpLeFwvTzlkTXk1Q00xNjBEd3VzVkpwdXl3YnBSaGNXcjNna3owUWdSTWRcclxuSVJWd3l4TmJBa0VBeWgzc2lwbWNnTjdTRDh4QkdcL010QllQcVdQMXZ4aFNWWVBmSnp1UFUzZ1M1TVJKelFIQnpcclxuc1ZDTGhUQlk3aEhTb3FpcWxxV1lhc2k4MUp6QkV3RXVRUUpCQUt3OXFHY1pqeU1IOEpVNVREU0dsbHIzanlieFxyXG5GRk1QajhUZ0pzMzQ2QUI4b3pxTExcL1RodldQcHhIdHRKYkg4UUFkTnV5V2RnNmRJZlZBYTk1aDdZK01DUUVaZ1xyXG5qUkRsMUJ6N2VXR08yYzBGcTlPVHozSVZMV3BubUd3ZlcrSHlheGl6eEZoVitGT2oxR1VWaXI5aHlsVjdWMERVXHJcblFqSWFqeXZcL29lRFdoRlE5d1FFQ1FDeWRoSjZOYU5RT0NaaCs2UVRySDNUQzVNZUJBMVllaXBvZTcrQmhzTE5yXHJcbmNGRzhzOXNUeFJubHRjWmwxZFhhQlNlbXZwTnZCaXpuMEt6aThHM1pBZ2M9XHJcbi0tLS0tRU5EIFJTQSBQUklWQVRFIEtFWS0tLS0tIiwicGMiOiItLS0tLUJFR0lOIENFUlRJRklDQVRFLS0tLS1cclxuTUlJQnNUQ0NBUm9DQ1FDQ1BqTnR0R05RV0RBTkJna3Foa2lHOXcwQkFRc0ZBREFkTVFzd0NRWURWUVFHRXdKQ1xyXG5SekVPTUF3R0ExVUVDZ3dGYlhsUVQxTXdIaGNOTVRneE1ERXlNRGN3T1RFeldoY05Namd4TURBNU1EY3dPVEV6XHJcbldqQWRNUXN3Q1FZRFZRUUdFd0pDUnpFT01Bd0dBMVVFQ2d3RmJYbFFUMU13Z1o4d0RRWUpLb1pJaHZjTkFRRUJcclxuQlFBRGdZMEFNSUdKQW9HQkFNTCtWVG1pWTR5Q2hvT1RNWlRYQUlHXC9tayt4ZlwvOW1qd0h4V3p4dEJKYk5uY05LXHJcbjBPTEkwVlhZS1cyR2dWa2xHSEhRanZldzFoVEZrRUdqbkNKN2Y1Q0RuYmd4ZXZ0eUFTREdzdDkyYTZ4Y0FlZEVcclxuYWRQMG5GWGhVeitjWVlJZ0ljZ2ZEY1gzWldlTkVGNWtzY3F5NTJrcEQyTzduRk5DVis4NXZTNGR1SkJOQWdNQlxyXG5BQUV3RFFZSktvWklodmNOQVFFTEJRQURnWUVBQ2oweGIrdE5ZRVJKa0wrcCt6RGNCc0JLNFJ2a25QbHBrK1lQXHJcbmVwaHVuRzJkQkdPbWdcL1dLZ29EMVBMV0QyYkVmR2dKeFlCSWc5cjF3TFlwREMxdHhoeFYrMk9CUVM4NktVTGgwXHJcbk5FY3IwcUVZMDVtSTRGbEUrRFwvQnBUXC8rV0Z5S2tadWc5MnJLMEZsejcxWHlcLzltQlhiUWZtK1lLNmw5cm9SWWRcclxuSjRzSGVRYz1cclxuLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLSIsImlkeCI6MX0=';
-        $cnf->loadConfigurationPackage($configurationPackage);
 
-        try{
-            $responce = \Mypos\IPC\Response::getInstance($cnf, $request, \Mypos\IPC\Defines::COMMUNICATION_FORMAT_POST);
-        }catch(\Mypos\IPC\IPC_Exception $e){
-            //Display Some general error or redirect to merchant store home page
+        $id = str_replace('toptim','', $request->get('OrderID'));
 
-            Log::info($responce);
-        }
+        $order = Order::query()->where('id', $id)->first();
 
-        $data = $responce->getData(CASE_LOWER);
+        $order->setData($id)->finish($request);
+
+        $order->update([
+            'order_status_id' => config('settings.order.new_status')
+        ]);
+
+        $this->forgetCheckoutCache();
+
+        return response()->json(['status' => 0, 'message' => 'OK']);
 
 
-
-
-        if ($data) {
-           /* $id    = substr($request->input('bill_id'), 18);
-            $order = Order::query()->where('id', $id)->first();
-
-            $order->setData($id)->finish($request);
-
-            $order->update([
-                'order_status_id' => config('settings.order.new_status')
-            ]);
-
-            $this->forgetCheckoutCache();
-
-            return response()->json(['status' => 0, 'message' => 'Accepted']);*/
-
-            Log::info($data);
-        }
-
-        return response()->json(['status' => 1, 'message' => 'Failed']);
     }
 
 
