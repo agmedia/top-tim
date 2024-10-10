a033e3f80bb7774a7607816643597aa1c3a35fa6
tomashina <tomislav@agmedia.hr>
2024-10-10 10:46:31 +0200

    widget backend fix - change translation->title to translation->name

--- resources/views/livewire/back/marketing/action-group-list.blade.php
+++ resources/views/livewire/back/marketing/action-group-list.blade.php
@@ -11,7 +11,7 @@
                         <div class="autocomplete" >
                             <div id="myInputautocomplete-list" class="autocomplete-items">
                                 @foreach($search_results as $item)
-                                    <div wire:click="addItem({{ $item->id }})">{{ isset($item->translation->title) ? $item->translation->title : $item->title }} - {{ isset($item->sku) ? $item->sku : '' }}</div>
+                                    <div wire:click="addItem({{ $item->id }})">{{ isset($item->translation->name) ? $item->translation->name : $item->name }} - {{ isset($item->sku) ? $item->sku : '' }}</div>
                                 @endforeach
                             </div>
                         </div>
