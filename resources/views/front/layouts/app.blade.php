968c5812b57ad5caf8eda7077b0766c6e7a0e553
tomashina <tomislav@agmedia.hr>
2024-10-11 14:49:13 +0200

    ga

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -25,8 +25,13 @@
     @if (config('app.env') == 'production')
         @yield('google_data_layer')
 
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5TDCJ29CB9"></script>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TFJG7JCX');</script>
+        <!-- End Google Tag Manager -->
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -34,7 +39,6 @@
 
             gtag('config', 'G-5TDCJ29CB9');
         </script>
-
     @endif
     @if (isset($js_lang))
         <script>
@@ -45,8 +49,12 @@
 </head>
 <!-- Body-->
 <body class="bg-secondary">
-<!-- Google Tag Manager (noscript) -->
-<!-- End Google Tag Manager (noscript) -->
+@if (config('app.env') == 'production')
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKZGQZV"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+@endif
 <div id="agapp">
     <div v-cloak>
         <div class="v-cloak--inline"> <!-- Parts that will be visible before compiled your HTML -->
