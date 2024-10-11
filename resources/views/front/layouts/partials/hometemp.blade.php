75fa5260f1bd004aaa649709a9e788a29e5ce177
tomashina <tomislav@agmedia.hr>
2024-10-11 09:04:57 +0200

    fix banner

--- resources/views/front/layouts/partials/hometemp.blade.php
+++ resources/views/front/layouts/partials/hometemp.blade.php
@@ -2,11 +2,11 @@
 <section class=" mb-0 bg-white">
         <!-- Item-->
         <div class="px-0 d-none d-md-block">
-            <img src="{{ asset('image/bannerd3-1.jpg') }}" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
+            <img src="{{ asset('image/bannerd3-1.webp') }}" width="1480" height="560" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
         </div>
 
     <div class="px-0 d-block d-md-none">
-        <img src="{{ asset('image/bannerd3-1-1000.jpg') }}" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
+        <img src="{{ asset('image/bannerd3-1-1000.webp') }}" width="600" height="600" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
     </div>
         <!-- Item-->
 </section>
