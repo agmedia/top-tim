97e89aafec60c4bf40ad5d7071ee3a08683e8a73
tomashina <tomislav@agmedia.hr>
2024-10-17 08:32:28 +0200

    forgot password

--- resources/views/emails/forget-password.blade.php
+++ resources/views/emails/forget-password.blade.php
@@ -13,7 +13,9 @@
             </td>
         </tr>
         <tr>
+            <td style="padding: 20px 20px 0 20px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
             <a href="{{ route('reset.password.get', $token) }}">{{ route('reset.password.get', $token) }}</a>
+            </td>
         </tr>
         <tr>
             <td style="padding: 20px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555; text-align: center;">
