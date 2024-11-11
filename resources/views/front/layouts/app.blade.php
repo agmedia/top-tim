f2e0ae30eae28460d60a3c3fbdcf2656bd077ea6
tomashina <tomislav@agmedia.hr>
2024-11-11 14:45:32 +0100

    app blade

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" media="screen" href="{{ asset('vendor/simplebar/dist/simplebar.min.css') }}"/>
     <link rel="stylesheet" media="screen" href="{{ asset('css/theme.css?v=2.95') }}">
+    <meta name="google-site-verification" content="A0oaGXU9FEbnLYZbkOpc0PXFLrFsN19VdLO_6em9r14" />
     <!-- Favicon and Touch Icons-->
     <link rel="apple-touch-icon" sizes="180x180" href="{{ config('settings.images_domain') . 'favicon-32x32.png' }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ config('settings.images_domain') . 'favicon-32x32.png' }}">
