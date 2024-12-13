76ac7e5e9b37856232a2ad53ded09e5448f7d515
tomashina <tomislav@agmedia.hr>
2024-12-13 11:28:03 +0100

    images

--- resources/views/front/layouts/partials/hometemp.blade.php
+++ resources/views/front/layouts/partials/hometemp.blade.php
@@ -1,11 +1,11 @@
 <!-- Hero slider-->
 <section class="tns-carousel mb-0  d-none d-md-block">
 
-    <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 1, &quot;mode&quot;: &quot;gallery&quot;, &quot;nav&quot;: true, &quot;responsive&quot;: {&quot;0&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: false}, &quot;576&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: true}}}">
+    <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 1,&quot;autoplay&quot;: true, &quot;autoplayTimeout&quot;: 4000, &quot;mode&quot;: &quot;gallery&quot;, &quot;nav&quot;: true, &quot;responsive&quot;: {&quot;0&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: false}, &quot;576&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: true}}}">
         <!-- Item-->
-        <div class="px-0 ">
+       <!-- <div class="px-0 ">
             <img src="{{ asset('media/img/1.jpg') }}" width="3308" height="1267" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
-        </div>
+        </div>-->
 
         <div class="px-0 ">
             <img src="{{ asset('media/img/2.jpg') }}" width="3308" height="1267" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
@@ -15,7 +15,7 @@
             <img src="{{ asset('image/bannerd3-1.webp') }}" width="3308" height="1267" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
         </div>
 
-
+        &quot;autoplay&quot;: true, &quot;autoplayTimeout&quot;: 3000
 
         <!-- Item-->
     </div>
@@ -24,11 +24,11 @@
 
 <section class="tns-carousel mb-0  d-block d-md-none">
 
-    <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 1, &quot;mode&quot;: &quot;gallery&quot;, &quot;nav&quot;: true, &quot;responsive&quot;: {&quot;0&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: false}, &quot;576&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: true}}}">
+    <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 1, &quot;autoplay&quot;: true, &quot;autoplayTimeout&quot;: 4000, &quot;mode&quot;: &quot;gallery&quot;, &quot;nav&quot;: true, &quot;responsive&quot;: {&quot;0&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: false}, &quot;576&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: true}}}">
 
-<div class="px-0 ">
+<!--<div class="px-0 ">
     <img src="{{ asset('media/img/1m.jpg') }}" width="600" height="600" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
-</div>
+</div>-->
 
 <div class="px-0 ">
     <img src="{{ asset('image/bannerd3-1-1000.webp') }}" width="600" height="600" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
