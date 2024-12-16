bc07d1071e8271a928f91391a489304bcf8d0738
tomashina <tomislav@agmedia.hr>
2024-12-16 08:30:38 +0100

    cart total

--- resources/views/front/layouts/partials/hometemp.blade.php
+++ resources/views/front/layouts/partials/hometemp.blade.php
@@ -3,9 +3,9 @@
 
     <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 1,&quot;autoplay&quot;: true, &quot;autoplayTimeout&quot;: 4000, &quot;mode&quot;: &quot;gallery&quot;, &quot;nav&quot;: true, &quot;responsive&quot;: {&quot;0&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: false}, &quot;576&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: true}}}">
         <!-- Item-->
-       <!-- <div class="px-0 ">
+      <div class="px-0 ">
             <img src="{{ asset('media/img/1.jpg') }}" width="3308" height="1267" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
-        </div>-->
+        </div>
 
         <div class="px-0 ">
             <img src="{{ asset('media/img/2.jpg') }}" width="3308" height="1267" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
@@ -26,9 +26,9 @@
 
     <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 1, &quot;autoplay&quot;: true, &quot;autoplayTimeout&quot;: 4000, &quot;mode&quot;: &quot;gallery&quot;, &quot;nav&quot;: true, &quot;responsive&quot;: {&quot;0&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: false}, &quot;576&quot;: {&quot;nav&quot;: true, &quot;controls&quot;: true}}}">
 
-<!--<div class="px-0 ">
+-<div class="px-0 ">
     <img src="{{ asset('media/img/1m.jpg') }}" width="600" height="600" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
-</div>-->
+</div>
 
 <div class="px-0 ">
     <img src="{{ asset('image/bannerd3-1-1000.webp') }}" width="600" height="600" alt="Top Tim - Better way to stay in the game" class="rounded-3"/>
