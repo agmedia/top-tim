cb51469eae1381a31061b6fe8445c7869db2fddd
Filip Jankoski <filip@agmedia.hr>
2025-01-15 10:06:02 +0100

    _fix: recaptcha on modal.

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -148,15 +148,14 @@
         let script = document.createElement('script');
         script.src = 'https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.sitekey') }}';
         head.appendChild(script);
-        setInterval(() => {
-            grecaptcha.ready(function() {
-                grecaptcha.execute('{{ config('services.recaptcha.sitekey') }}', {action: 'register'}).then(function(token) {
-                    if (token) {
-                        document.getElementById('recaptcha').value = token;
-                    }
-                });
+
+        grecaptcha.ready(function() {
+            grecaptcha.execute('{{ config('services.recaptcha.sitekey') }}', {action: 'register'}).then(function(token) {
+                if (token) {
+                    document.getElementById('recaptcha').value = token;
+                }
             });
-        }, 270);
+        });
     })
 </script>
 @stack('js_after')
