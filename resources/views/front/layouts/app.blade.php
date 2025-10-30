6b1c5ad2d1b5a39f3e15290f72acbd350cfa431a
tomashina <tomislav@agmedia.hr>
2025-10-30 16:03:48 +0100

    fix price minus

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <base href="{{ config('settings.images_domain') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" media="screen" href="{{ asset('vendor/simplebar/dist/simplebar.min.css') }}"/>
-    <link rel="stylesheet" media="screen" href="{{ asset('css/theme.css?v=2.96') }}">
+    <link rel="stylesheet" media="screen" href="{{ asset('css/theme.css?v=2.97') }}">
     <meta name="google-site-verification" content="A0oaGXU9FEbnLYZbkOpc0PXFLrFsN19VdLO_6em9r14" />
     <!-- Favicon and Touch Icons-->
     <link rel="apple-touch-icon" sizes="180x180" href="{{ config('settings.images_domain') . 'favicon-32x32.png' }}">
